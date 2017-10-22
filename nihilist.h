7fba65217d174c7325c532591eb25673df4538d0
Matt Wanner <mwanner@ufl.edu>
2017-10-22 01:50:50 -0400

    initial commit

---
+++ nihilist.h
@@ -0,0 +1,17 @@
+#ifndef PA3_H
+#define PA3_H
+#include <string>
+
+using namespace std;
+
+string nihilist(string k1, string k2, string s, bool encrypt);
+
+string uppercase(string s);
+
+string lowercase(string s);
+
+string removeNonLetters(string s);
+
+string removeNonNumbers(string s);
+
+#endif
